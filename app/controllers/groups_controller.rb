ab06166bdfe4b1bfb232eb793022720624b8655e
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-29 18:31:44 +0200

    Add styling

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -53,7 +53,9 @@ class GroupsController < ApplicationController
 
   # DELETE /groups/1 or /groups/1.json
   def destroy
+
     @group.destroy
+    
 
     respond_to do |format|
       format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
